5b6a6f6b5b98c32427d3e6992091036ff89308e5
oleveloper <oleveloper@gmail.com>
2022-08-25 21:50:08 +0900

    Data management with Django admin

--- board/models.py
+++ board/models.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 class Post(models.Model):
     title = models.TextField()
     contents = models.TextField()
     author = models.TextField()
+    is_public = models.BooleanField(default=True, verbose_name='공개 여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
