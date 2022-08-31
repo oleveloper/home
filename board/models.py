55b7be5367df01713f70c050b35c07c614464df4
oleveloper <oleveloper@gmail.com>
2022-08-31 22:21:18 +0900

    Add media settings

--- board/models.py
+++ board/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.TextField()
     contents = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='board/post/%Y/%m/%d')
     author = models.TextField()
     is_public = models.BooleanField(default=True, verbose_name='공개 여부')
     created_at = models.DateTimeField(auto_now_add=True)
